dd2a41eafbc004bdb1a631cb08fb1d1ea54c4478
Lands <380788433@qq.com>
2018-11-04 17:36:44 +0800

    测试

--- myProxy.py
+++ myProxy.py
@@ -2,22 +2,22 @@
 
 import socket
 
+
 def proxy():
+    ip = "127.0.0.1"
+    port = 9800
     sock = socket.socket()
-    sock.bind(("127.0.0.1", 9800))
+    sock.bind((ip, port))
     sock.listen(5)
+    print "[*] listening on %s:%d" % (ip, port)
 
     while True:
         print 'server waiting...'
         conn, addr = sock.accept()
-        response = b''
 
         client_data = conn.recv(1024)
-        while client_data:
-            response += client_data
-            client_data = conn.recv(1024)
-        print response
-        conn.sendall(response.encode('utf-8'))
+        response = str('test')
+        sock.sendall(response)
 
 if __name__ == '__main__':
     proxy()
