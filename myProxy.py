ae25112777341ef472ec2ac48ec52decbcd27607
Lands <380788433@qq.com>
2018-11-07 17:28:59 +0800

    现在已经可以连接了，只是http请求格式有问题

--- myProxy.py
+++ myProxy.py
@@ -26,15 +26,19 @@ class Proxy:
             print self.client_data
             req_url = re.findall('http\S+', self.client_data)
             if len(req_url):
-                self.request_url = req_url[0]
-            response = self.client_proxy()
-            conn.sendall(response)
+                need_clean_url = req_url[0]
+                _req_url = re.sub('http://', '', need_clean_url)
+                cleaned_url = re.sub('/', '', _req_url)
+                self.request_url = cleaned_url
+                response = self.client_proxy()
+                conn.sendall(response)
             conn.close()
 
     def client_proxy(self):
         url = self.request_url
+        print url
         port = 80
-        client_socket = socket.socket()
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((url, port))
         client_socket.send(self.client_data.encode())
         response = b''
@@ -43,9 +47,25 @@ class Proxy:
             response += rec
             rec = client_socket.recv(1024)
         print(response.decode())
+        client_socket.close()
         return response.decode()
 
 if __name__ == '__main__':
-    ip = '127.0.0.1'
-    port = 9800
-    proxy = Proxy(ip, port)
+    # ip = '127.0.0.1'
+    # port = 9800
+    # proxy = Proxy(ip, port)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    url = "element-cn.eleme.io"
+    port = 80
+    client_socket.connect((url, port))
+    client_socket.send('GET / HTTP/1.1\r\nHost: element-cn.eleme.io\r\nConnection: close\r\n\r\n')
+    buffer = []
+    while True:
+        # 每次最多接收1k字节:
+        d = client_socket.recv(1024)
+        if d:
+            buffer.append(d)
+        else:
+            break
+    data = ''.join(buffer)
+    print data
